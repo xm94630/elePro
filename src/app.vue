0f346b4844f862ea2a9cb500a186a4337708f264
xm94630 <xm94630@126.com>
2017-07-19 14:06:58 +0800

    按钮组件运用，非常的简单

--- src/app.vue
+++ src/app.vue
@@ -5,13 +5,25 @@
     <el-row :gutter="15">
       <el-col :span="4">
         <div class="grid-content bg1">
-          我是左侧区域
+          <div>我是左侧区域</div>
+          <i class="el-icon-edit"></i>
+          <i class="el-icon-share"></i>
+          <i class="el-icon-delete"></i>
         </div>
       </el-col>
       <el-col :span="20">
         <div class="grid-content bg1">
           <div>我是右侧的区域</div>
+          
           <el-button @click="visible = true">按钮</el-button>
+          <el-button type="primary" icon="search">搜索</el-button>
+          <el-button type="success" icon="edit" size="small">编辑</el-button>
+          <el-button type="success" :loading=true >我的加载</el-button>
+          <el-button-group>
+            <el-button type="primary" icon="edit" size="small">左按钮</el-button>
+            <el-button type="primary" icon="edit" size="small">右按钮</el-button>
+          </el-button-group>
+
           <el-dialog v-model="visible" title="Hello world">
               <p>欢迎使用 Element</p>
           </el-dialog>
@@ -27,6 +39,7 @@
     &:last-child {
       margin-bottom: 0;
     }
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   }
   .el-col {
     border-radius: 4px;
