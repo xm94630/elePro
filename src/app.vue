cbe3e4d61fd3b733cc8a33862710199942104514
xm94630 <xm94630@126.com>
2017-07-19 10:57:00 +0800

    仿照完成第一个案例。（注意data要在模板处export）

--- src/app.vue
+++ src/app.vue
@@ -2,11 +2,18 @@
   <div>
     <h1>elePro</h1>
     <p>A vue project.</p>
+    <el-button @click="visible = true">按钮</el-button>
+    <el-dialog v-model="visible" title="Hello world">
+        <p>欢迎使用 Element</p>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
-  };
+export default {
+  name: 'app',
+  data: function() {
+    return { visible: false };
+  }
+};
 </script>
