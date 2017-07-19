b4e3bb81916d074e728f6f0123f6487568f136c8
xm94630 <xm94630@126.com>
2017-07-19 10:10:29 +0800

    原来这个cooking只是一个vue项目的脚手架工具，本身并不包含UI组件，我这里单独引入了。另外这里是有语法测试的，缺少分号啥的就会报错

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './app';
+import 'element-ui/lib/theme-default/index.css';
+import ElementUI from 'element-ui';
 
+Vue.use(ElementUI);
 new Vue({ // eslint-disable-line
   el: '#app',
   render: h => h(App)
