db3cb91e396a86ba44336ecec494038e757255ba
xm94630 <xm94630@126.com>
2017-09-20 11:33:23 +0800

    解决了一个大坑：在使用ele 菜单组件的时候打算启用路由的时候遇到一些障碍，this. 为undefined，只需要实例化的时候添加一个属性是关键

--- src/app.vue
+++ src/app.vue
@@ -35,7 +35,7 @@
     <el-row :gutter="0" class="conBox">
       <el-col :span="3" class="menuBox">
         <!--左侧菜单-->
-        <el-menu default-active="2" class="el-menu-vertical-demo collapsed" @open="handleOpen" @close="handleClose" @select="leftMenu">
+        <el-menu default-active="2" class="el-menu-vertical-demo collapsed" @open="handleOpen" @close="handleClose" @select="leftMenu" router>
               <el-submenu index="1">
                 <template slot="title"><i class="el-icon-message"></i>出装</template>
                 <el-menu-item-group>
@@ -225,7 +225,14 @@
 
 
 <script>
+import Vue from 'vue';
+import router from './router';
 export default {
+  router,
+  beforeCreate(){
+    console.log('看看 this.$route 是不是已经存在啦，关键在于把 router 引入')
+    console.log(this.$route)
+  },
   name: 'app',
   data: function() {
     return {
